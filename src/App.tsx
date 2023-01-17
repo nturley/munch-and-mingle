2a6c2db40fcbb15e200ccb0d9e74d34db9ab879d
nturley <gitea@fake.local>
2023-01-16 18:40:07 -0700

    use hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,17 @@
-import './App.css'
-import { useState } from 'react';
-import { auth} from './firebase'
-import { EmailAuthProvider, User } from "firebase/auth";
-import { StyledFirebaseAuth } from 'react-firebaseui';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import EventList from './EventList';
-import Event from './Event';
-import { eventLoader } from './Event';
-
-
-const uiConfig = {
-  signInFlow: 'popup',
-  signInSuccessUrl : '/',
-  signInOptions: [
-    EmailAuthProvider.PROVIDER_ID,
-  ],
-};
-
-
-
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <EventList/>,
-  },
-  {
-    path: "/event/:eventId",
-    element: <Event/>,
-    loader: eventLoader,
-  },
-  {
-    path: "/login",
-    element: <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth} />,
-  },
-]);
-
+import "./App.css";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import Event from "./Event";
 
 function App() {
-  
   return (
     <div className="App">
-      <RouterProvider router={router} />      
+      <HashRouter>
+        <Routes>
+          <Route path="/event/:eventId" element={<Event/>}/>
+        </Routes>
+      </HashRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Event.tsx
+++ src/Event.tsx
@@ -1,8 +1,8 @@
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { db} from './firebase'
 import { collection, doc, getDoc, getDocs, setDoc } from 'firebase/firestore';
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, useParams } from 'react-router-dom';
 import { EventData, PollResult } from './EventList';
 import DatePicker, { DateObject } from "react-multi-date-picker";
 import { useCollection } from 'react-firebase-hooks/firestore';
@@ -11,8 +11,6 @@ export async function eventLoader({ params}: {params: any}) {
   const docRef = doc(db, "events", params.eventId);
   const docSnap = await getDoc(docRef);
   if (docSnap.exists()) {
-    //const pollResults = await getDocs(collection(db, `events/${params.eventId}/pollResults`));
-    //const pollData = pollResults.docs.map(d => ({...d.data(), name: d.id}))
     return {...docSnap.data(), id: params.eventId}
   } else {
     throw new Error('No such document!')
@@ -20,21 +18,36 @@ export async function eventLoader({ params}: {params: any}) {
 }
 
 export default function Event() {
+  const { eventId } = useParams();
   const [value, setValue] = useState<DateObject|null|DateObject[]>(null);
   const [name, setName] = useState<string>('')
-  const myEvent = useLoaderData() as EventData
+  const [myEvent, setMyEvent] = useState<EventData|null>(null)
+  const [pollResults] = useCollection(collection(db, `events/${eventId}/pollResults`))
 
-  const [pollResults] = useCollection(collection(db, `events/${myEvent.id}/pollResults`))
+  
+
+  async function getEvent() {
+    if (eventId === undefined) throw new Error('No event id')
+    const docRef = doc(db, "events", eventId);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      setMyEvent({...docSnap.data(), id: eventId} as EventData)
+    } else {
+      throw new Error('No such document!')
+    }
+  }
+  useEffect(() => getEvent(), [eventId])
+  
   const pollData: PollResult[] = pollResults?.docs.map(d => ({...d.data(), name: d.id} as PollResult)) || []
   const allDays: string[] = Array.from(new Set(pollData.flatMap(p => p.dates.split(',').map(d => d.trim())))).sort()
   function submitForm(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault()
     const dates = (value as DateObject[]).map(v => v.toString())
-    setDoc(doc(db, `events/${myEvent.id}/pollResults`, name), {
+    setDoc(doc(db, `events/${eventId}/pollResults`, name), {
       dates: dates.join(',')
     })
   }
-
+  if (myEvent === null) return (<></>)
   return (<>
     <h2>{myEvent.name}</h2>
     <form onSubmit={(e) => submitForm(e)}>
